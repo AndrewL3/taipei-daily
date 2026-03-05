a2fc49b2f6b991a004f1d684bd6613d4c3109353
Andrew <andrewlin20@gmail.com>
2026-03-05 14:54:51 +0800

    feat: add module collection entry point for module registry system

---
+++ apps/web/src/modules/index.ts
@@ -0,0 +1,8 @@
+import { registerModules } from "../core/module-registry";
+
+// Modules are registered here. Each module exports a ModuleDefinition.
+// Task 3 will add: import { garbageModule } from './garbage';
+registerModules([
+  // garbageModule,  // Task 3
+  // youbikeModule,  // Task 5
+]);
